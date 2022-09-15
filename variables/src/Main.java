bff69ea92a7a8d24f544c7bf7ffc412b73a3ab89
Ceyda Önal <66831721+ceydaonal11@users.noreply.github.com>
2022-09-15 17:07:59 +0300

    Add files via upload
    Değişkenler

---
+++ variables/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+                int ogrenciSayisi = 12;
+                String mesaj ="Öğrenci Sayısı: ";
+                System.out.println(mesaj + ogrenciSayisi);
+                System.out.println("Öğrenci Sayısı: " +ogrenciSayisi );
+         
+    }
+}
