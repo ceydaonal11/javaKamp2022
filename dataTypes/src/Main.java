ae9a878800a8cfeba9b4ef337648508d4f631b46
Ceyda Önal <66831721+ceydaonal11@users.noreply.github.com>
2022-09-15 17:22:17 +0300

    Add files via upload

---
+++ dataTypes/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int sayi = 11;
+        sayi = 12; // Tam sayısal ifadeler default olarak int kabul edilir.
+        // byte sayi = -45;
+        // double sayi = 7.37;
+        char karakter = 'C';
+        boolean dogruMu = false; // True ya da False tutar.
+
+    }
+}
