1859987201fa88c1a37e5dd86afaca5322629fba
Ceyda Önal <66831721+ceydaonal11@users.noreply.github.com>
2022-09-15 17:07:10 +0300

    Delete variables.java

--- variables.java
+++
@@ -1,8 +0,0 @@
-public class variables {
-    public static void main(String[] args) {
-         int ogrenciSayisi = 12;
-         String mesaj ="Öğrenci Sayısı: ";
-        System.out.println(mesaj + ogrenciSayisi);
-        System.out.println("Öğrenci Sayısı: " +ogrenciSayisi );
-    }
-}
